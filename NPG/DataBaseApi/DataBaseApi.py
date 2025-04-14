c7abd77ea000cce8c3bf28bc2e3fcef2ca5faf8c
KiviGibson <77674694+KiviGibson@users.noreply.github.com>
2025-04-14 14:22:18 +0200

    add pressure query builder

--- NPG/DataBaseApi/DataBaseApi.py
+++ NPG/DataBaseApi/DataBaseApi.py
@@ -13,3 +13,28 @@ class PressuereData:
     value: float
     desc: str
 
+class PressureQuery:
+    def __init__(self):
+        query: str = "SELECT * FROM pressure"
+
+    def addDateRange(self, f: int, t: int = f) -> PressureQuery:
+        if !(query.find("WHERE")):
+            query += f" WHERE date between ({f}, {t})"
+        else:
+            query += f" AND date BETWEEN ({f}, {t})"
+        return self
+
+    def addPressureRange(f: float, t: float = f) -> PressureQuery:
+        if query.find("WHERE"):
+            query += f" WHERE date BETWEEN ({f}, {t})"
+        else:
+            query += f" AND date BETWEEN ({f}, {t})"
+        return self
+    
+    def addLimit(self, amount: int, first: int = 0) - > PressureQuery:
+        query += f" LIMIT {amount}, {first}"
+        return self
+
+    def build() -> str:
+        return query + ";"
+
