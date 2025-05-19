6cfce15ec7fddd2889902b50a1a466803a349ef6
KiviGibson <77674694+KiviGibson@users.noreply.github.com>
2025-05-19 20:18:38 +0200

    repleace auth to better place

--- NPG/DataBaseApi/Auth.py
+++ NPG/DataBaseApi/Auth.py
@@ -1,5 +1,5 @@
 from jwt import encode, decode
-from ..DataBaseApi import Database
+from ..DataBaseApi.DataBaseApi import Database
 
 
 class Auth:
@@ -7,15 +7,28 @@ class Auth:
         self._db = db
 
     def checkCredentials(self, jwt: str) -> int:
-        return 0
+        jwt = decode(jwt, "passphrase", algorithm="HS256")
+        print(jwt)
+        return -1
 
     def login(self, login: str, password: str) -> str | int:
         id = self._db.getUser(login, password)
         if id == -1:
-            return ""
+            return -1
         return encode(
             {"login": login, "password": password}, "passphrase", algorithm="HS256"
         )
 
     def register(self, login: str, password: str) -> str | int:
-        return ""
+        id = self._db.createUser(login, password)
+        if id == -1:
+            return -1
+        return self.login(login, password)
+
+
+if __name__ == "__main__":
+    d = Database(":memory:")
+    a = Auth(d)
+    jwt = a.register("Kivi", "Gibson")
+    if jwt is str:
+        a.checkCredentials(jwt)
